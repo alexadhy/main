8706896c5a78f9cf7fa826bea9783d73222935a1
alexadhy <alex.dhyatma@ubuntusoftware.net>
2021-04-06 13:10:45 +0000

    removed useless print statements

--- deploy/templates/maintemplatev2/client/lib/core/routing/route_guard.dart
+++ deploy/templates/maintemplatev2/client/lib/core/routing/route_guard.dart
@@ -4,11 +4,9 @@ import 'package:sys_share_sys_account_service/view/widgets/view_model/auth_nav_v
 class NavRailGuard implements RouteGuard {
   @override
   Future<bool> canActivate(String path, ModularRoute router) async {
-    print("CURRENT ROUTE: $path");
     var authNavVm = Modular.get<AuthNavViewModel>();
     await Future.delayed(Duration.zero, () {
       final navIdx = authNavVm.getDynamicNavIndex(path);
-      print("CURRENT NAV IDX: $navIdx");
       if (navIdx > 0) {
         authNavVm.setCurrentNavIndex(navIdx);
       }
